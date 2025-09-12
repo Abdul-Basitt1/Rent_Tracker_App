40551a1cee9d9fdb2e852b957332d914c8a3b7de
Abdul Basit <abdulrocker4@gmail.com>
2025-09-12 17:06:43 +0500

    Added simple onBoarding

--- src/Screens/Auth/OnBoarding/OnBoarding.tsx
+++ src/Screens/Auth/OnBoarding/OnBoarding.tsx
@@ -1,15 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { AuthStackParamList } from '../../../Navigation/types'
+import { MainContainer } from '../../../Components/MainContainer'
+import { Images } from '../../../Images'
+import Onboarding from 'react-native-onboarding-swiper'
 
 type OnBoardingProps = NativeStackScreenProps<AuthStackParamList, 'OnBoarding'>
 
 const OnBoarding: React.FC<OnBoardingProps> = ({ navigation }) => {
     return (
-        <View>
-            <Text>OnBoarding</Text>
-        </View>
+        <MainContainer>
+            <Onboarding
+                pages={[
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image source={Images.splashLogo} />,
+                        title: 'Onboarding',
+                        subtitle: 'Done with React Native Onboarding Swiper',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image source={Images.splashLogo} />,
+                        title: 'Onboarding2',
+                        subtitle: 'Done with React Native Onboarding Swiper',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image source={Images.splashLogo} />,
+                        title: 'Onboarding3',
+                        subtitle: 'Done with React Native Onboarding Swiper',
+                    },
+                ]}
+            />
+        </MainContainer>
     )
 }
 
